462963a065324a3875977d6444f20635c018e7b8
NubeDev <aidan@enviroservices.com.au>
2022-05-27 12:40:35 +1000

    added api calls

--- rubix-ui/network.go
+++ rubix-ui/network.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/NubeIO/rubix-assist-client/nube/assist"
 	"github.com/NubeIO/rubix-assist-model/model"
 )
 
@@ -17,5 +18,29 @@ func (app *App) GetHostNetworks() (resp []model.Network) {
 	client := app.initRest()
 	data, res := client.GetHostNetworks()
 	fmt.Println("GetHostNetworks", res.AsString())
+	return data
+}
+
+func (app *App) DeleteHostNetwork(uuid string) *assist.Response {
+	client := app.initRest()
+	res := client.DeleteHostNetwork(uuid)
+	fmt.Println("DeleteHost", res.GetStatus(), uuid)
+	return res
+}
+
+func (app *App) GetHostNetwork(uuid string) *model.Network {
+	client := app.initRest()
+	data, res := client.GetHostNetwork(uuid)
+	fmt.Println("GetHost", res.GetStatus(), data.UUID)
+	return data
+}
+
+func (app *App) EditHostNetwork(uuid string, host *model.Network) *model.Network {
+	client := app.initRest()
+	if host == nil {
+		return nil
+	}
+	data, res := client.UpdateHostNetwork(uuid, host)
+	fmt.Println("EditHost", res.GetStatus(), host.UUID)
 	return data
 }
