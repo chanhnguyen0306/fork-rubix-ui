a2fa80c3d8aa203b9fdbb96d4fc6217ee62c4234
NubeDev <aidan@enviroservices.com.au>
2022-05-27 11:10:49 +1000

    added api calls

--- rubix-ui/frontend/wailsjs/go/main/App.d.ts
+++ rubix-ui/frontend/wailsjs/go/main/App.d.ts
@@ -3,6 +3,12 @@
 import {model} from '../models';
 import {assist} from '../models';
 
+export function AddHostNetwork(arg1:model.Network):Promise<model.Network>;
+
+export function DeleteHost(arg1:string):Promise<assist.Response>;
+
+export function EditHost(arg1:string,arg2:model.Host):Promise<model.Host>;
+
 export function GetHost(arg1:string):Promise<model.Host>;
 
 export function GetHostNetworks():Promise<Array<model.Network>>;
@@ -10,9 +16,3 @@ export function GetHostNetworks():Promise<Array<model.Network>>;
 export function GetHosts():Promise<Array<model.Host>>;
 
 export function AddHost(arg1:model.Host):Promise<model.Host>;
-
-export function AddHostNetwork(arg1:model.Network):Promise<model.Network>;
-
-export function DeleteHost(arg1:string):Promise<assist.Response>;
-
-export function EditHost(arg1:string,arg2:model.Host):Promise<model.Host>;
