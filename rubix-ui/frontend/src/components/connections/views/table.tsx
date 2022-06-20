cc2d9244433132eca3e1e7d54d1672edf4576569
Oanh Nguyen <52970495+nguyenoanh5597@users.noreply.github.com>
2022-06-20 09:40:51 +0700

    fix bug call api for Connections

--- rubix-ui/frontend/src/components/connections/views/table.tsx
+++ rubix-ui/frontend/src/components/connections/views/table.tsx
@@ -1,96 +1,80 @@
-import {useNavigate} from "react-router-dom";
-import {Space, Spin, Table} from "antd";
-import {DeleteConnection} from "../../../../wailsjs/go/main/App";
-import {storage} from "../../../../wailsjs/go/models";
+import { useNavigate } from "react-router-dom";
+import { Space, Spin, Table } from "antd";
+import { DeleteConnection } from "../../../../wailsjs/go/main/App";
+import { storage } from "../../../../wailsjs/go/models";
 import RubixConnection = storage.RubixConnection;
 
 export const ConnectionsTable = (props: any) => {
-    const {
-        connections,
-        updateConnections,
-        showModal,
-        isFetching,
-        setIsFetching,
-    } = props;
-    if (!connections) return <></>;
+  const { connections, refreshList, showModal, isFetching } = props;
+  if (!connections) return <></>;
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const columns = [
-        {
-            title: "uuid",
-            dataIndex: "uuid",
-            key: "uuid",
-        },
-        {
-            title: "Name",
-            dataIndex: "name",
-            key: "name",
-        },
-        {
-            title: "Description",
-            dataIndex: "description",
-            key: "description",
-        },
-        {
-            title: "Address",
-            dataIndex: "ip",
-            key: "ip",
-        },
-        {
-            title: "Port",
-            dataIndex: "port",
-            key: "port",
-        },
-        // {
-        //   title: "Locations",
-        //   dataIndex: "locations",
-        //   key: "locations",
-        //   render: (locations: []) => <a>{locations ? locations.length : 0}</a>,
-        // },
-        {
-            title: "Actions",
-            dataIndex: "actions",
-            key: "actions",
-            render: (_: any, conn: RubixConnection) => (
-                <Space size="middle">
-                    <a onClick={() => navigate(`locations/${conn.uuid}`)}>View</a>
-                    <a
-                        onClick={() => {
-                            showModal(conn);
-                        }}
-                    >
-                        Edit
-                    </a>
-                    <a
-                        onClick={() => {
-                            deleteConnection(conn.uuid);
-                        }}
-                    >
-                        Delete
-                    </a>
-                </Space>
-            ),
-        },
-    ];
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "Description",
+      dataIndex: "description",
+      key: "description",
+    },
+    {
+      title: "Address",
+      dataIndex: "ip",
+      key: "ip",
+    },
+    {
+      title: "Port",
+      dataIndex: "port",
+      key: "port",
+    },
+    {
+      title: "uuid",
+      dataIndex: "uuid",
+      key: "uuid",
+    },
+    {
+      title: "Actions",
+      dataIndex: "actions",
+      key: "actions",
+      render: (_: any, conn: RubixConnection) => (
+        <Space size="middle">
+          <a onClick={() => navigate(`locations/${conn.uuid}`)}>View</a>
+          <a
+            onClick={() => {
+              showModal(conn);
+            }}
+          >
+            Edit
+          </a>
+          <a
+            onClick={() => {
+              deleteConnection(conn.uuid);
+            }}
+          >
+            Delete
+          </a>
+        </Space>
+      ),
+    },
+  ];
 
-    const deleteConnection = async (uuid: string) => {
-        await DeleteConnection(uuid);
-        const newConnections = connections.filter(
-            (c: RubixConnection) => c.uuid !== uuid
-        );
-        updateConnections(newConnections);
-        setIsFetching(true);
-    };
+  const deleteConnection = async (uuid: string) => {
+    await DeleteConnection(uuid);
+    refreshList();
+  };
 
-    return (
-        <div>
-            <Table
-                rowKey="uuid"
-                dataSource={connections}
-                columns={columns}
-                loading={{ indicator: <Spin />, spinning: isFetching }}
-            />
-        </div>
-    );
+  return (
+    <div>
+      <Table
+        rowKey="uuid"
+        dataSource={connections}
+        columns={columns}
+        loading={{ indicator: <Spin />, spinning: isFetching }}
+      />
+    </div>
+  );
 };
