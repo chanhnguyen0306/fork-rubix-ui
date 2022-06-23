bee8952cc54967b4b85fc2ed0f470b623daa2fba
NubeDev <aidan@enviroservices.com.au>
2022-06-23 15:18:22 +1000

    added networking page and redo location and networks to components

--- rubix-ui/frontend/src/components/locations/locations.tsx
+++ rubix-ui/frontend/src/components/locations/locations.tsx
@@ -1,126 +1,171 @@
-import {
-    AddLocation,
-    GetLocation,
-    GetLocations,
-    GetLocationSchema,
-    GetLocationTableSchema,
-    UpdateLocation
-} from "../../../wailsjs/go/main/App";
-import {model} from "../../../wailsjs/go/models";
-import {Helpers} from "../../helpers/checks";
+import {useEffect, useState} from "react";
+import {useNavigate, useParams} from "react-router-dom";
+import {LocationFactory} from "./factory";
+import {DeleteLocation, GetConnection, GetLocations} from "../../../wailsjs/go/main/App";
+import {isObjectEmpty} from "../../utils/utils";
+import {Space} from "antd";
+import {model, storage} from "../../../wailsjs/go/models";
+import RubixConnection = storage.RubixConnection;
+import {AddButton, CreateEditModal} from "./views/create";
+import {LocationsTable} from "./views/table";
+import Location = model.Location;
 
+export const Locations = () => {
+    const [locations, setLocations] = useState([] as Location[]);
+    const [currentLocation, setCurrentLocation] = useState({} as Location);
+    const [locationSchema, setLocationSchema] = useState({});
+    const [tableSchema, setTableSchema] = useState([]);
+    const [connection, setConnection] = useState({} as RubixConnection);
+    const [isModalVisible, setIsModalVisible] = useState(false);
+    const [isFetching, setIsFetching] = useState(true);
+    const [isLoadingForm, setIsLoadingForm] = useState(false);
 
-function hasUUID(uuid: string): Error {
-    return Helpers.IsUndefined(uuid, "Location or connection uuid") as Error
-}
+    let { connUUID } = useParams();
+    const navigate = useNavigate();
+    let locationFactory = new LocationFactory();
+    locationFactory.connectionUUID = connUUID as string;
 
-export class LocationFactory {
+    useEffect(() => {
+        fetchList();
+        getSchemaTable();
+    }, []); //on first load hook react
 
-    uuid!: string;
-    connectionUUID!: string;
-    private _this!: model.Location;
-    private count!: number
-    private _all!:Array<model.Location>;
+    useEffect(() => {
+        getConnection();
+    }, [connUUID]); //on load when connUUID changes
 
-    get all(): Array<model.Location> {
-        return this._all;
-    }
+    const fetchList = async () => {
+        try {
+            setIsFetching(true);
+            let res = await GetLocations(locationFactory.connectionUUID);
+            res = !res ? [] : res;
+            setLocations(res);
+        } catch (error) {
+            setLocations([]);
+        } finally {
+            setIsFetching(false);
+        }
+    };
 
-    set all(value: Array<model.Location>) {
-        this._all = value;
-    }
+    const getConnection = async () => {
+        try {
+            const res = await GetConnection(locationFactory.connectionUUID);
+            setConnection(res);
+        } catch (error) {
+            console.log(error);
+        } finally {
+        }
+    };
 
-    get this(): model.Location {
-        return this._this;
-    }
+    const getSchema = async () => {
+        setIsLoadingForm(true);
+        let res = await locationFactory.Schema();
+        res = {
+            properties: {
+                ...res.properties,
+                connection_name: {
+                    title: "Connection",
+                    type: "string",
+                    default: connection.name,
+                    readOnly: true,
+                },
+            },
+        };
+        setLocationSchema(res);
+        setIsLoadingForm(false);
+    };
 
-    set this(value: model.Location) {
-        this._this = value;
-    }
+    const deleteLocation = async (uuid: string) => {
+        await DeleteLocation(locationFactory.connectionUUID, uuid);
+        refreshList();
+        setIsFetching(true);
+    };
 
-    public GetTotalCount(): number {
-        return this.count
-    }
+    const refreshList = () => {
+        fetchList();
+    };
 
+    const onShowModal = (location: Location) => {
+        setCurrentLocation(location);
+        setIsModalVisible(true);
+        if (isObjectEmpty(locationSchema)) {
+            getSchema();
+        }
+    };
 
-    async Schema(): Promise<any> {
-        let all: Promise<any> = {} as Promise<any>
-        await GetLocationSchema(this.connectionUUID).then(res => {
-            all = res as Promise<any>
-        }).catch(err => {
-            return undefined
-        })
-        return all
-    }
+    const onCloseModal = () => {
+        setIsModalVisible(false);
+    };
 
-    async TableSchema(): Promise<any> {
-        let all: Promise<any> = {} as Promise<any>
-        await GetLocationTableSchema(this.connectionUUID).then(res => {
-            all = res as Promise<any>
-        }).catch(err => {
-            return undefined
-        })
-        return all
-    }
+    const getSchemaTable = async () => {
+        try {
+            const r = await locationFactory.TableSchema();
+            let tableSchema = r;
+            tableSchema = [
+                ...tableSchema,
+                {
+                    title: "Networks count",
+                    dataIndex: "networks",
+                    key: "networks",
+                    render: (networks: []) => <a>{networks ? networks.length : 0}</a>,
+                },
+                {
+                    title: "Actions",
+                    dataIndex: "actions",
+                    key: "actions",
+                    render: (_: any, location: Location) => (
+                        <Space size="middle">
+                            <a
+                                onClick={() =>
+                                    navigate(`/networks/${location.uuid}`, {
+                                        state: { connUUID: connUUID },
+                                    })
+                                }
+                            >
+                                View
+                            </a>
+                            <a
+                                onClick={() => {
+                                    onShowModal(location);
+                                }}
+                            >
+                                Edit
+                            </a>
+                            <a
+                                onClick={() => {
+                                    deleteLocation(location.uuid);
+                                }}
+                            >
+                                Delete
+                            </a>
+                        </Space>
+                    ),
+                },
+            ];
+            setTableSchema(tableSchema);
+        } catch (error) {}
+    };
 
-
-    // get the first connection uuid
-    async GetFist(): Promise<model.Location> {
-        let one: model.Location = {} as model.Location
-        await this.GetAll().then(res => {
-            one = res.at(0) as model.Location
-            this._this = one
-        }).catch(err => {
-            return undefined
-        })
-        return one
-    }
-
-    async GetAll(): Promise<Array<model.Location>> {
-        let all: Array<model.Location> = {} as Array<model.Location>
-        await GetLocations(this.connectionUUID).then(res => {
-            all = res as Array<model.Location>
-            this.all = all
-        }).catch(err => {
-            return undefined
-        })
-        return all
-    }
-
-    async GetOne(): Promise<model.Location> {
-        hasUUID(this.uuid)
-        let one: model.Location = {} as model.Location
-        await GetLocation(this.connectionUUID, this.uuid).then(res => {
-            one = res as model.Location
-            this._this = one
-        }).catch(err => {
-            return undefined
-        })
-        return one
-    }
-
-    async Add(): Promise<model.Location> {
-        hasUUID(this.uuid)
-        let one: model.Location = {} as model.Location
-        await AddLocation(this.connectionUUID, this._this).then(res => {
-            one = res as model.Location
-            this._this = one
-        }).catch(err => {
-            return undefined
-        })
-        return one
-    }
-
-    async Update(): Promise<model.Location> {
-        hasUUID(this.uuid)
-        let one: model.Location = {} as model.Location
-        await UpdateLocation(this.connectionUUID, this.uuid, this._this).then(res => {
-            one = res as model.Location
-            this._this = one
-        }).catch(err => {
-            return undefined
-        })
-        return one
-    }
-
-}
+    return (
+        <>
+            <h2>Locations</h2>
+            <AddButton onShowModal={onShowModal} />
+            <CreateEditModal
+                locations={locations}
+                currentLocation={currentLocation}
+                locationSchema={locationSchema}
+                isModalVisible={isModalVisible}
+                isLoadingForm={isLoadingForm}
+                refreshList={refreshList}
+                onCloseModal={onCloseModal}
+                setIsFetching={setIsFetching}
+                connUUID={connUUID}
+            />
+            <LocationsTable
+                locations={locations}
+                isFetching={isFetching}
+                tableSchema={tableSchema}
+            />
+        </>
+    );
+};
