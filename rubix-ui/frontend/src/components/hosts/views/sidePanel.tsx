f99d0c4c2e9b4bbea4394f0911194fff321dd2ab
Oanh Nguyen <52970495+nguyenoanh5597@users.noreply.github.com>
2022-07-21 18:24:07 +0700

    styling side panel

--- rubix-ui/frontend/src/components/hosts/views/sidePanel.tsx
+++ rubix-ui/frontend/src/components/hosts/views/sidePanel.tsx
@@ -92,7 +92,7 @@ export const SidePanel = (props: any) => {
     >
       <div style={{ display: collapsed ? "none" : "block" }}>
         <h4 style={{ margin: "10px" }}>{selectedHost.name}</h4>
-        <Card title="Rubix-Wires">
+        <Card title="Rubix-Wires" className="rubix-wires-card">
           <Row style={actionRow}>
             <Col span={10}>
               <Button
