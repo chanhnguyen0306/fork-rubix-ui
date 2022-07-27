f59d5895c9dca4fe8d5de8b0790a2280b4269938
Oanh Nguyen <52970495+nguyenoanh5597@users.noreply.github.com>
2022-07-27 16:45:35 +0700

    use rbActions for backups

--- rubix-ui/frontend/src/components/backups/views/table.tsx
+++ rubix-ui/frontend/src/components/backups/views/table.tsx
@@ -1,93 +1,83 @@
-import {Button, notification, Popconfirm, Spin} from "antd";
-import {EventsOn} from "../../../../wailsjs/runtime";
+import { Spin } from "antd";
 import RbTable from "../../../common/rb-table";
-import React, {useState} from "react";
-import {main} from "../../../../wailsjs/go/models";
-import {DeleteOutlined} from "@ant-design/icons";
-import {LogFactory} from "../../logs/factory";
-import {BackupFactory} from "../factory";
+import { useState } from "react";
+import { main } from "../../../../wailsjs/go/models";
+import { BackupFactory } from "../factory";
+import { RbDeleteButton } from "../../../common/rb-table-actions";
 export const BackupsTable = (props: any) => {
-    const {data, isFetching, fetch} = props;
-    const [selectedUUIDs, setSelectedUUIDs] = useState([] as Array<main.UUIDs>);
-    let backupFactory = new BackupFactory();
+  const { data, isFetching, fetch } = props;
+  const [selectedUUIDs, setSelectedUUIDs] = useState([] as Array<main.UUIDs>);
+  let backupFactory = new BackupFactory();
 
-    const rowSelection = {
-        onChange: (selectedRowKeys: any, selectedRows: any) => {
-            setSelectedUUIDs(selectedRows);
-        },
-    };
+  const rowSelection = {
+    onChange: (selectedRowKeys: any, selectedRows: any) => {
+      setSelectedUUIDs(selectedRows);
+    },
+  };
 
-    const bulkDelete = async () => {
-        await backupFactory.BulkDelete(selectedUUIDs);
-        fetch()
+  const bulkDelete = async () => {
+    await backupFactory.BulkDelete(selectedUUIDs);
+    fetch();
+  };
 
-    };
+  const columns = [
+    {
+      title: "uuid",
+      dataIndex: "uuid",
+      key: "uuid",
+    },
+    {
+      title: "connection name",
+      dataIndex: "connection_name",
+      key: "connection_name",
+    },
+    {
+      title: "connection uuid",
+      dataIndex: "connection_uuid",
+      key: "connection_uuid",
+    },
+    {
+      title: "host name",
+      dataIndex: "host_name",
+      key: "host_name",
+    },
+    {
+      title: "host uuid",
+      dataIndex: "host_uuid",
+      key: "host_uuid",
+    },
+    {
+      title: "timestamp",
+      dataIndex: "time",
+      key: "time",
+    },
+    {
+      title: "application",
+      dataIndex: "application",
+      key: "application",
+    },
+    {
+      title: "info",
+      dataIndex: "backup_info",
+      key: "backup_info",
+    },
+    {
+      title: "Comments",
+      dataIndex: "user_comment",
+      key: "user_comment",
+    },
+  ];
 
-    const columns = [
-        {
-            title: "uuid",
-            dataIndex: "uuid",
-            key: "uuid",
-        },
-        {
-            title: "connection name",
-            dataIndex: "connection_name",
-            key: "connection_name",
-        },
-        {
-            title: "connection uuid",
-            dataIndex: "connection_uuid",
-            key: "connection_uuid",
-        },
-        {
-            title: "host name",
-            dataIndex: "host_name",
-            key: "host_name",
-        },
-        {
-            title: "host uuid",
-            dataIndex: "host_uuid",
-            key: "host_uuid",
-        },
-        {
-            title: "timestamp",
-            dataIndex: "time",
-            key: "time",
-        },
-        {
-            title: "application",
-            dataIndex: "application",
-            key: "application",
-        },
-        {
-            title: "info",
-            dataIndex: "backup_info",
-            key: "backup_info",
-        },
-        {
-            title: "Comments",
-            dataIndex: "user_comment",
-            key: "user_comment",
-        },
-    ];
-
-
-
-    return (
-        <>
-            <Popconfirm title="Delete" onConfirm={bulkDelete}>
-                <Button type="primary" danger style={{ margin: "5px", float: "right" }}>
-                    <DeleteOutlined /> Delete
-                </Button>
-            </Popconfirm>
-            <RbTable
-                rowKey="uuid"
-                dataSource={data}
-                columns={columns}
-                rowSelection={rowSelection}
-                loading={{indicator: <Spin/>, spinning: isFetching}}
-            />
-        </>
-
-    );
+  return (
+    <>
+      <RbDeleteButton bulkDelete={bulkDelete} />
+      <RbTable
+        rowKey="uuid"
+        dataSource={data}
+        columns={columns}
+        rowSelection={rowSelection}
+        loading={{ indicator: <Spin />, spinning: isFetching }}
+      />
+    </>
+  );
 };
