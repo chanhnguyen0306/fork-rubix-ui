edf8067455b4d8585142aed80fc52dd1e3b0e79c
NubeDev <aidan@enviroservices.com.au>
2022-07-28 05:34:53 +1000

    redo relases

--- rubix-ui/backend/store/releases.go
+++ rubix-ui/backend/store/releases.go
@@ -12,8 +12,10 @@ type Release struct {
 	Uuid                string   `json:"uuid"`
 	Name                string   `json:"name"`
 	Repo                string   `json:"repo"`
+	MinVersion          string   `json:"min_version"`
 	Release             string   `json:"release"`
 	Port                int      `json:"port"`
+	Transport           string   `json:"transport"`
 	Products            []string `json:"products"`
 	Arch                []string `json:"arch"`
 	AppSpecficExecStart string   `json:"app_specfic_exec_start"`
@@ -21,31 +23,36 @@ type Release struct {
 		Name                   string   `json:"name"`
 		Repo                   string   `json:"repo"`
 		Description            string   `json:"description"`
+		Port                   int      `json:"port,omitempty"`
+		Transport              string   `json:"transport"`
 		Products               []string `json:"products"`
 		Arch                   []string `json:"arch"`
 		Version                string   `json:"version"`
+		MinVersion             string   `json:"min_version,omitempty"`
 		MaxVersion             string   `json:"max_version"`
 		FlowDependency         bool     `json:"flow_dependency"`
-		PluginDependency       string   `json:"plugin_dependency"`
-		Port                   int      `json:"port,omitempty"`
-		ServiceDependency      []string `json:"service_dependency,omitempty"`
+		PluginDependency       []string `json:"plugin_dependency"`
+		ServiceDependency      []string `json:"service_dependency"`
 		CustomServiceExecStart string   `json:"custom_service_exec_start,omitempty"`
 	} `json:"apps"`
 	Plugins []struct {
 		Name              string   `json:"name"`
 		Plugin            string   `json:"plugin"`
 		Description       string   `json:"description"`
-		Products          []string `json:"products,omitempty"`
-		Arch              []string `json:"arch,omitempty"`
+		Products          []string `json:"products"`
+		Arch              []string `json:"arch"`
 		AppDependency     []string `json:"app_dependency"`
 		PluginDependency  []string `json:"plugin_dependency"`
 		ServiceDependency []string `json:"service_dependency"`
+		Port              int      `json:"port,omitempty"`
+		Transport         string   `json:"transport,omitempty"`
 	} `json:"plugins"`
 	Services []struct {
 		Name        string   `json:"name"`
 		ServiceName string   `json:"service_name"`
 		Description string   `json:"description"`
 		Port        int      `json:"port"`
+		Transport   string   `json:"transport"`
 		Products    []string `json:"products"`
 		Arch        []string `json:"arch"`
 	} `json:"services"`
