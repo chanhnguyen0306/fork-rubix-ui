0302900e60e3d6b045706d1b13c05951d095a050
Oanh Nguyen <52970495+nguyenoanh5597@users.noreply.github.com>
2022-08-01 18:47:13 +0700

    save settings

--- rubix-ui/frontend/src/components/settings/views/token-modal.tsx
+++ rubix-ui/frontend/src/components/settings/views/token-modal.tsx
@@ -1,36 +1,46 @@
 import { useEffect, useState } from "react";
 import { Input, Modal } from "antd";
 import { SettingsFactory } from "../factory";
-import { storage } from "../../../../wailsjs/go/models";
+import { getDarkMode } from "../../../themes/use-theme";
+import { openNotificationWithIcon } from "../../../utils/utils";
 
 export const TokenModal = (props: any) => {
   const { isModalVisible, onClose } = props;
   const [confirmLoading, setConfirmLoading] = useState(false);
-  const [body, setBody] = useState("");
-
+  const [token, setToken] = useState("");
+  const _darkMode = getDarkMode();
   const factory = new SettingsFactory();
 
   useEffect(() => {
-    setBody("");
+    GetSetting();
   }, [isModalVisible]);
 
   const onChange = (
     e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
   ) => {
-    setBody(e.target.value);
+    setToken(e.target.value);
+  };
+
+  const GetSetting = async () => {
+    try {
+      // const setttings = await factory.Get();
+      const git_token = await factory.GitToken();
+      git_token ? setToken(git_token) : setToken("");
+    } catch (error) {
+      console.log(error);
+    } finally {
+    }
   };
 
   const handleOk = async () => {
     try {
       setConfirmLoading(true);
-      const payload = { theme: "dark", git_token: body };
-      const res = await factory.Update(payload);
-      // const res = await factory.Get();
-      // const res = await factory.GitToken();
-      console.log(res);
-
+      const payload = { theme: _darkMode ? "dark" : "light", git_token: token };
+      await factory.Update(payload);
+      openNotificationWithIcon("success", "Update Token Successful!");
       onClose();
     } catch (error) {
+      setToken("");
       console.log(error);
     } finally {
       setConfirmLoading(false);
@@ -47,7 +57,11 @@ export const TokenModal = (props: any) => {
       onCancel={onClose}
       confirmLoading={confirmLoading}
     >
-      <Input placeholder="please enter token..." onChange={onChange} />
+      <Input
+        placeholder="please enter token..."
+        onChange={onChange}
+        value={token}
+      />
     </Modal>
   );
 };
