2b0f1e81339bd0b93c4ca8fc2ebc8f2fa792a7c2
NubeDev <aidan@enviroservices.com.au>
2022-08-18 15:52:49 +1000

    fix bacnet whois and discover point

--- rubix-ui/frontend/src/components/hosts/host/flow/bacnet/factory.tsx
+++ rubix-ui/frontend/src/components/hosts/host/flow/bacnet/factory.tsx
@@ -1,53 +1,19 @@
 import {model} from "../../../../../../wailsjs/go/models";
 import {BacnetWhois, GetBacnetDevicePoints} from "../../../../../../wailsjs/go/main/App";
-import {Helpers} from "../../../../../helpers/checks";
-
-function hasUUID(uuid: string): Error {
-    return Helpers.IsUndefined(uuid, "host or connection uuid") as Error
-}
 
 
 export class BacnetFactory {
     hostUUID!: string;
     connectionUUID!: string;
-    uuid!: string;
-    _this!: model.Point;
-
-    get this(): model.Point {
-        return this._this;
-    }
-
-    set this(value: model.Point) {
-        this._this = value;
-    }
-
-    async Whois(bacnetNetworkUUID:string, pluginName:string): Promise<Array<model.Device>> {
-        hasUUID(this.connectionUUID)
-        hasUUID(this.hostUUID)
-        let all: Promise<Array<model.Device>> = {} as Promise<Array<model.Device>>
-        await BacnetWhois(this.connectionUUID, this.hostUUID, bacnetNetworkUUID, pluginName).then(res => {
-            all = res as unknown as Promise<Array<model.Device>>
-        }).catch(err => {
-            console.log("bacnet err", err)
-            return undefined
-        })
-        return all
-    }
 
 
-    async DiscoverDevicePoints(deviceUUID:string, addPoints:boolean, makeWriteable:boolean): Promise<Array<model.Point>> {
-        hasUUID(this.connectionUUID)
-        hasUUID(this.hostUUID)
-        makeWriteable = true
-        let all: Promise<Array<model.Point>> = {} as Promise<Array<model.Point>>
-        await GetBacnetDevicePoints(this.connectionUUID, this.hostUUID, deviceUUID, addPoints, makeWriteable).then(res => {
-            all = res as unknown as Promise<Array<model.Point>>
-        }).catch(err => {
-            console.log("bacnet err", err)
-            return undefined
-        })
-        return all
-    }
+  async Whois(bacnetNetworkUUID:string, pluginName:string): Promise<Array<model.Device>> {
+    return await BacnetWhois(this.connectionUUID, this.hostUUID, bacnetNetworkUUID, pluginName);
+  }
+
+  async DiscoverDevicePoints(deviceUUID:string, addPoints:boolean, makeWriteable:boolean): Promise<Array<model.Point>> {
+    return await GetBacnetDevicePoints(this.connectionUUID, this.hostUUID, deviceUUID, addPoints, makeWriteable);
+  }
 
 
 }
