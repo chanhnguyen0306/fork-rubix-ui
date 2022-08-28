646456cc180ee4ae7538ddb7c9e515588185ec05
Oanh Nguyen <52970495+nguyenoanh5597@users.noreply.github.com>
2022-08-28 10:36:39 +0700

    change table pagination position

--- rubix-ui/frontend/src/common/rb-table.tsx
+++ rubix-ui/frontend/src/common/rb-table.tsx
@@ -4,7 +4,11 @@ import Table from "antd/lib/table";
 const DISPLAY_MAX_WIDTH = 1024;
 
 const RbTable = (props: any) => (
-  <Table {...props} scroll={{ x: DISPLAY_MAX_WIDTH }} />
+  <Table
+    {...props}
+    scroll={{ x: DISPLAY_MAX_WIDTH }}
+    pagination={{ position: ["bottomLeft"] }}
+  />
 );
 
 export default RbTable;
