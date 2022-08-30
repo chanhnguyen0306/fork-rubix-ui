b9eb437b45a435c86db9153060f2392816d8240b
NubeDev <aidan@enviroservices.com.au>
2022-08-30 18:01:22 +1000

    fix get system time

--- rubix-ui/system.go
+++ rubix-ui/system.go
@@ -9,25 +9,6 @@ import (
 
 var nets = networking.New()
 
-func (inst *App) GetHostTime(connUUID, hostUUID string) interface{} {
-	client, err := inst.initConnection(&AssistClient{ConnUUID: connUUID})
-	if err != nil {
-		inst.crudMessage(false, fmt.Sprintf("error %s", err.Error()))
-		return nil
-	}
-	data, err := client.ProxyGET(hostUUID, "/api/system/time")
-	if err != nil {
-		inst.crudMessage(false, fmt.Sprintf("error %s", err.Error()))
-		return nil
-	}
-	if data.IsError() {
-		inst.crudMessage(false, fmt.Sprintf("error %s", data.Error()))
-		return nil
-	}
-	d := humanize.Map(data.Body())
-	return d
-}
-
 func (inst *App) GetServerTime(connUUID string) interface{} {
 	client, err := inst.initConnection(&AssistClient{ConnUUID: connUUID})
 	if err != nil {
