6693d9d260043b05ca5da7cc7a720868525cb8ef
Oanh Nguyen <52970495+nguyenoanh5597@users.noreply.github.com>
2022-09-05 14:14:01 +0700

    styling

--- rubix-ui/frontend/src/common/rb-table.tsx
+++ rubix-ui/frontend/src/common/rb-table.tsx
@@ -3,16 +3,19 @@ import Table from "antd/lib/table";
 
 const DISPLAY_MAX_WIDTH = 1024;
 
-const RbTable = (props: any) => (
-  <Table
-    {...props}
-    scroll={{ x: DISPLAY_MAX_WIDTH }}
-    pagination={{
-      position: ["bottomLeft"],
-      showSizeChanger: true,
-      pageSizeOptions: [10, 50],
-    }}
-  />
-);
+const RbTable = (props: any) => {
+  return (
+    <Table
+      {...props}
+      scroll={{ x: DISPLAY_MAX_WIDTH }}
+      pagination={{
+        position: ["bottomLeft"],
+        showSizeChanger: true,
+        pageSizeOptions: [10, 50, 100, 1000],
+        locale: { items_per_page: "" },
+      }}
+    />
+  );
+};
 
 export default RbTable;
