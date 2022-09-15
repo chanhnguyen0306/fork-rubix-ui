70bb86f7d8c2930a226956c22753407b4fd98862
NubeDev <aidan@enviroservices.com.au>
2022-09-15 15:40:12 +1000

    updated download node

--- rubix-ui/floweng.go
+++ rubix-ui/floweng.go
@@ -5,6 +5,7 @@ import (
 	"github.com/NubeDev/flow-eng/nodes"
 	"github.com/NubeIO/rubix-rules/flow"
 	"github.com/NubeIO/rubix-ui/flowcli"
+	"github.com/mitchellh/mapstructure"
 )
 
 func (inst *App) NodePallet() []nodes.PalletNode {
@@ -19,15 +20,18 @@ func (inst *App) NodePallet() []nodes.PalletNode {
 
 func (inst *App) DownloadFlow(encodedNodes interface{}, restartFlow bool) *flow.Message {
 	var client = flowcli.New(nil)
-	nodesList, ok := encodedNodes.(*nodes.NodesList)
-	if !ok {
-		inst.crudMessage(false, fmt.Sprintf("failed to decode nodes"))
+	nodeList := &nodes.NodesList{}
+	err := mapstructure.Decode(encodedNodes, &nodeList)
+	if err != nil {
+		inst.crudMessage(false, fmt.Sprintf("error %s", err.Error()))
 		return nil
 	}
-	downloadFlow, err := client.DownloadFlow(nodesList, restartFlow)
+	downloadFlow, err := client.DownloadFlow(nodeList, restartFlow)
 	if err != nil {
 		inst.crudMessage(false, fmt.Sprintf("error %s", err.Error()))
 		return downloadFlow
+	} else {
+		inst.crudMessage(true, fmt.Sprintf(downloadFlow.Message))
 	}
 	return downloadFlow
 }
