6d88d9f9abb43ae391b39dffd6c717423233b591
RaiBnod <binodrayee@gmail.com>
2022-09-29 10:59:36 +0545

    Fix: config update issue on BACnet Server C

--- rubix-ui/base.go
+++ rubix-ui/base.go
@@ -27,7 +27,6 @@ const bacnetServerDriver = "bacnet-server-driver"
 const bacnetMasterDriver = "bacnet-master-driver"
 const flowFramework = "flow-framework"
 const rubixWires = "rubix-wires"
-const wiresBuilds = "wires-builds"
 
 func (inst *App) errMsg(err error) error {
 	if err != nil {
@@ -80,7 +79,7 @@ func (inst *App) initConnection(body *AssistClient) (*assitcli.Client, error) {
 		err = inst.errMsg(err)
 		return nil, errors.New("conn can not be empty")
 	}
-	inst.mutex.Lock() // mutex was added had issue with "fatal error: concurrent map writes"
+	inst.mutex.Lock() // mutex was added had the issue with "fatal error: concurrent map writes"
 	defer inst.mutex.Unlock()
 	if connUUID == "" {
 		err = inst.errMsg(errors.New("conn can not be empty"))
