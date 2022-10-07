4c953b0107c276336e8edb982acd11b667a0091e
Chanh Nguyen <chanh.nguyen0306@digitalfortress.dev>
2022-10-08 02:17:00 +0700

    Number, Strings and Bools #183

--- rubix-ui/frontend/src/components/rubix-flow/components/InputSocket.tsx
+++ rubix-ui/frontend/src/components/rubix-flow/components/InputSocket.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import { CaretRightOutlined } from "@ant-design/icons";
 import {
   Connection,
@@ -17,6 +18,34 @@ export type InputSocketProps = {
   onChange: (key: string, value: any) => void;
 } & InputSocketSpecJSON;
 
+const REGEX_NUMBER = new RegExp('^$|^-?(\\d+)?(\\.?\\d*)?$');
+
+const getValueOptions = (value: number) => {
+  switch (value) {
+    case 0:
+      return true;
+    case 1:
+      return false;
+    case 2:
+      return null;
+    default:
+      return false;
+  }
+};
+
+const getNumberOptions = (value: boolean) => {
+  switch (value) {
+    case true:
+      return 0;
+    case false:
+      return 1;
+    case null:
+      return 2;
+    default:
+      return 1;
+  }
+};
+
 export const InputSocket = ({
   connected,
   value,
@@ -25,12 +54,29 @@ export const InputSocket = ({
   valueType,
 }: InputSocketProps) => {
   const instance = useReactFlow();
+  const [inputNumber, setInputNumber] = useState("");
+
   const showFlowIcon = valueType === "flow";
   const colorName = valueTypeColorMap[valueType];
   const [backgroundColor, borderColor] = colors[colorName];
 
   const onChangeInputNumber = (e: React.FormEvent<HTMLInputElement>) => {
-    const value = e.currentTarget.value ? Number(e.currentTarget.value) : null;
+    setInputNumber(e.currentTarget.value);
+  };
+
+  const onBlurInputNumber = (e: React.FormEvent<HTMLInputElement>) => {
+    if (inputNumber.match(REGEX_NUMBER)) {
+      onChange(name, Number(inputNumber));
+    } else if (inputNumber === "null") {
+      onChange(name, "null");
+    } else {
+      setInputNumber("0");
+      onChange(name, 0);
+    }
+  };
+
+  const onChangeCheckbox = (e: React.FormEvent<HTMLSelectElement>) => {
+    const value = getValueOptions(Number((e.target as HTMLInputElement).value));
     onChange(name, value);
   };
 
@@ -48,25 +94,30 @@ export const InputSocket = ({
                 <AutoSizeInput
                   type="text"
                   className=" bg-gray-600 disabled:bg-gray-700 py-1 px-2 nodrag"
-                  value={value}
+                  value={value || "null"}
                   onChange={(e) => onChange(name, e.currentTarget.value)}
                 />
               )}
               {valueType === "number" && (
                 <AutoSizeInput
-                  type="number"
+                  type="text"
                   className=" bg-gray-600 disabled:bg-gray-700 py-1 px-2 nodrag"
-                  value={String(value)}
+                  value={inputNumber}
                   onChange={(e) => onChangeInputNumber(e)}
+                  onBlur={(e) => onBlurInputNumber(e)}
                 />
               )}
               {valueType === "boolean" && (
-                <input
-                  type="checkbox"
-                  className=" bg-gray-600 disabled:bg-gray-700 py-1 px-2 nodrag"
-                  value={value}
-                  onChange={(e) => onChange(name, e.currentTarget.checked)}
-                />
+                <select
+                  value={getNumberOptions(value)}
+                  className="bg-gray-600 disabled:bg-gray-700 py-1 px-2 nodrag"
+                  onChange={onChangeCheckbox}
+                  style={{ paddingRight: 18 }}
+                >
+                  <option value="0">true</option>
+                  <option value="1">false</option>
+                  <option value="2">null</option>
+                </select>
               )}
             </>
           )}
