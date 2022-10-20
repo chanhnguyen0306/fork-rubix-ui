6a0059c74dd109808596e76e0c534feb9d943f27
hanh.tv204 <hanhtran@Hanhs-MacBook-Pro.local>
2022-10-20 14:24:37 +0700

    chore: remove old dist folder

--- rubix-ui/frontend/dist/assets/dark-theme.75cea872.js
+++
@@ -1,1 +0,0 @@
-import{j as a,F as r}from"./index.c6b2df42.js";const t=()=>a(r,{});export{t as default};
--- rubix-ui/frontend/dist/assets/light-theme.1f67b977.js
+++
@@ -1,1 +0,0 @@
-import{j as a,F as t}from"./index.c6b2df42.js";const r=()=>a(t,{});export{r as default};
