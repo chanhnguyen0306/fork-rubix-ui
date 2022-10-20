aad5266ca95c20a19c57488446f3c1e92dbafe6d
RaiBnod <binodrayee@gmail.com>
2022-10-20 10:57:54 +0545

    Remove dist and build files

--- frontend/dist/assets/dark-theme.d22b95ef.js
+++
@@ -1,1 +0,0 @@
-import{j as a,F as r}from"./index.4d933c7e.js";const t=()=>a(r,{});export{t as default};
--- frontend/dist/assets/light-theme.b31e02d0.js
+++
@@ -1,1 +0,0 @@
-import{j as a,F as t}from"./index.4d933c7e.js";const r=()=>a(t,{});export{r as default};
