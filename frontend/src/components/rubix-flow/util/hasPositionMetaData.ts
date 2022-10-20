51e653b695f81d3be28664eea094635ec0ccb2a5
hanh.tv204 <hanhtran@Hanhs-MacBook-Pro.local>
2022-10-20 14:15:11 +0700

    fix: show output 0 when data null

--- frontend/src/components/rubix-flow/util/hasPositionMetaData.ts
+++ frontend/src/components/rubix-flow/util/hasPositionMetaData.ts
@@ -1,6 +1,10 @@
 import { GraphJSON } from "../lib";
 
 export const hasPositionMetaData = (graph: GraphJSON): boolean => {
+  if (!graph.nodes) {
+    return false;
+  }
+
   return graph.nodes.some(
     (node) =>
       node.metadata?.positionX !== undefined ||
---
+++ rubix-ui/frontend/dist/assets/light-theme.1f67b977.js
@@ -0,0 +1,1 @@
+import{j as a,F as t}from"./index.c6b2df42.js";const r=()=>a(t,{});export{r as default};
