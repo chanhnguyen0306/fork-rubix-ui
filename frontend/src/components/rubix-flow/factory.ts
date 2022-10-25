5019bfd377eda0d54b45f47511d0fa24ee85deb2
Oanh Nguyen <52970495+nguyenoanh5597@users.noreply.github.com>
2022-10-25 12:25:37 +0700

    #256 add flow on host

--- frontend/src/components/rubix-flow/factory.ts
+++ frontend/src/components/rubix-flow/factory.ts
@@ -1,5 +1,6 @@
 import {
-  AddWiresConnection, BulkDeleteWiresConnection,
+  AddWiresConnection,
+  BulkDeleteWiresConnection,
   DeleteWiresConnection,
   DownloadFlow,
   GetFlow,
@@ -9,64 +10,71 @@ import {
   NodeSchema,
   NodeValue,
   NodeValues,
-  UpdateWiresConnection
+  UpdateWiresConnection,
 } from "../../../wailsjs/go/backend/App";
-import {db, node} from "../../../wailsjs/go/models";
+import { db, node } from "../../../wailsjs/go/models";
 
 export class FlowFactory {
-
   // arg1 is the connectionUUID
   // arg2 is hostUUID
   // arg3 is used when user is programming on flow localhost (as in running rubix-edge-wires backend on their PC)
   // set arg3 to true if its a remote connection
 
-
-
-  async BulkDeleteWiresConnection(uuids: Array<string>):Promise<any> {
-    return await BulkDeleteWiresConnection("", "",false, uuids);
+  async BulkDeleteWiresConnection(uuids: Array<string>): Promise<any> {
+    return await BulkDeleteWiresConnection("", "", false, uuids);
   }
 
   async GetWiresConnections(): Promise<Array<db.Connection>> {
-    return await GetWiresConnections("", "",false);
+    return await GetWiresConnections("", "", false);
+  }
+
+  async GetWiresConnectionsRemote(
+    connUUID: string,
+    hostUUID: string
+  ): Promise<Array<db.Connection>> {
+    return await GetWiresConnections(connUUID, hostUUID, true);
   }
 
   async GetWiresConnection(uuid: string): Promise<db.Connection> {
-    return await GetWiresConnection("", "",false, uuid);
+    return await GetWiresConnection("", "", false, uuid);
   }
 
   async DeleteWiresConnection(uuid: string) {
-    await DeleteWiresConnection("", "",false, uuid);
+    await DeleteWiresConnection("", "", false, uuid);
   }
 
-  async UpdateWiresConnection(uuid: string, body: db.Connection): Promise<db.Connection> {
-    return await UpdateWiresConnection("", "",false, uuid, body);
+  async UpdateWiresConnection(
+    uuid: string,
+    body: db.Connection
+  ): Promise<db.Connection> {
+    return await UpdateWiresConnection("", "", false, uuid, body);
   }
 
   async AddWiresConnection(body: db.Connection): Promise<db.Connection> {
-    return await AddWiresConnection("", "",false, body);
+    return await AddWiresConnection("", "", false, body);
   }
 
   async NodeValue(nodeUUID: string): Promise<node.Values> {
-    return await NodeValue("", "",false, nodeUUID);
+    return await NodeValue("", "", false, nodeUUID);
   }
 
   async NodeSchema(nodeName: string) {
-    return await NodeSchema("", "",false, nodeName);
+    return await NodeSchema("", "", false, nodeName);
   }
 
   async NodeValues(): Promise<Array<node.Values>> {
-    return await NodeValues("", "",false);
+    return await NodeValues("", "", false);
   }
 
   async GetFlow(): Promise<any> {
-    return await GetFlow("", "",false);
+    return await GetFlow("", "", false);
   }
 
   async NodePallet() {
-    return await NodePallet("", "",false);
+    return await NodePallet("", "", false);
   }
 
   async DownloadFlow(encodedNodes: any, restartFlow: boolean) {
-    return await DownloadFlow("", "",false, encodedNodes, restartFlow);
+    return await DownloadFlow("", "", false, encodedNodes, restartFlow);
   }
 }
