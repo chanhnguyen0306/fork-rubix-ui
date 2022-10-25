435a416b6e74fc7f225deba1b6c1922df765ceaf
NubeDev <aidan@enviroservices.com.au>
2022-10-25 17:10:23 +1100

    add in all args

--- frontend/src/components/rubix-flow/factory.ts
+++ frontend/src/components/rubix-flow/factory.ts
@@ -20,60 +20,61 @@ export class FlowFactory {
   // arg3 is used when user is programming on flow localhost (as in running rubix-edge-wires backend on their PC)
   // set arg3 to true if its a remote connection
 
-  async BulkDeleteWiresConnection(uuids: Array<string>): Promise<any> {
-    return await BulkDeleteWiresConnection("", "", false, uuids);
+  async BulkDeleteWiresConnection(connUUID:string, hostUUID:string, isRemote:boolean, uuids: Array<string>): Promise<any> {
+    return await BulkDeleteWiresConnection(connUUID, hostUUID, isRemote, uuids);
   }
 
   async GetWiresConnections(
     connUUID: string,
-    hostUUID: string
+    hostUUID: string,
+    isRemote:boolean,
   ): Promise<Array<db.Connection>> {
     const _connUUID = connUUID || "";
     const _hostUUID = hostUUID || "";
-    const isRemote = connUUID && hostUUID ? true : false;
-    return await GetWiresConnections(_connUUID, _hostUUID, isRemote);
+    return await GetWiresConnections(connUUID, hostUUID, isRemote);
   }
 
-  async GetWiresConnection(uuid: string): Promise<db.Connection> {
-    return await GetWiresConnection("", "", false, uuid);
+  async GetWiresConnection(connUUID:string, hostUUID:string, isRemote:boolean, uuid: string): Promise<db.Connection> {
+    return await GetWiresConnection(connUUID, hostUUID, isRemote, uuid);
   }
 
-  async DeleteWiresConnection(uuid: string) {
-    await DeleteWiresConnection("", "", false, uuid);
+  async DeleteWiresConnection(connUUID:string, hostUUID:string, isRemote:boolean, uuid: string) {
+    await DeleteWiresConnection(connUUID, hostUUID, isRemote, uuid);
   }
 
   async UpdateWiresConnection(
+    connUUID:string, hostUUID:string, isRemote:boolean,
     uuid: string,
     body: db.Connection
   ): Promise<db.Connection> {
-    return await UpdateWiresConnection("", "", false, uuid, body);
+    return await UpdateWiresConnection(connUUID, hostUUID, isRemote, uuid, body);
   }
 
-  async AddWiresConnection(body: db.Connection): Promise<db.Connection> {
-    return await AddWiresConnection("", "", false, body);
+  async AddWiresConnection(connUUID:string, hostUUID:string, isRemote:boolean, body: db.Connection): Promise<db.Connection> {
+    return await AddWiresConnection(connUUID, hostUUID, isRemote, body);
   }
 
-  async NodeValue(nodeUUID: string): Promise<node.Values> {
-    return await NodeValue("", "", false, nodeUUID);
+  async NodeValue(connUUID:string, hostUUID:string, isRemote:boolean, nodeUUID: string): Promise<node.Values> {
+    return await NodeValue(connUUID, hostUUID, isRemote, nodeUUID);
   }
 
-  async NodeSchema(nodeName: string) {
-    return await NodeSchema("", "", false, nodeName);
+  async NodeSchema(connUUID:string, hostUUID:string, isRemote:boolean, nodeName: string) {
+    return await NodeSchema(connUUID, hostUUID, isRemote, nodeName);
   }
 
-  async NodeValues(): Promise<Array<node.Values>> {
-    return await NodeValues("", "", false);
+  async NodeValues(connUUID:string, hostUUID:string, isRemote:boolean): Promise<Array<node.Values>> {
+    return await NodeValues(connUUID, hostUUID, isRemote);
   }
 
-  async GetFlow(): Promise<any> {
-    return await GetFlow("", "", false);
+  async GetFlow(connUUID:string, hostUUID:string, isRemote:boolean): Promise<any> {
+    return await GetFlow(connUUID, hostUUID, isRemote);
   }
 
-  async NodePallet() {
-    return await NodePallet("", "", false);
+  async NodePallet(connUUID:string, hostUUID:string, isRemote:boolean) {
+    return await NodePallet(connUUID, hostUUID, isRemote);
   }
 
-  async DownloadFlow(encodedNodes: any, restartFlow: boolean) {
-    return await DownloadFlow("", "", false, encodedNodes, restartFlow);
+  async DownloadFlow(connUUID:string, hostUUID:string, isRemote:boolean, encodedNodes: any, restartFlow: boolean) {
+    return await DownloadFlow(connUUID, hostUUID, isRemote, encodedNodes, restartFlow);
   }
 }
