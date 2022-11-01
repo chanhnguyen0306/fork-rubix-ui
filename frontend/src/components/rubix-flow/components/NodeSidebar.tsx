3add103bc8601436a9bb8dcdacb01d013cadf76b
hanh.tran <hanh.tv204@gmail.com>
2022-11-01 10:34:52 +0700

    feat: Implement collapse for node into category

--- frontend/src/components/rubix-flow/components/NodeSidebar.tsx
+++ frontend/src/components/rubix-flow/components/NodeSidebar.tsx
@@ -1,17 +1,24 @@
-import { useState } from "react";
-import { useReactFlow } from "react-flow-renderer";
+import { ChangeEvent, useEffect, useState } from "react";
+import { Collapse } from "antd";
+
 import { NodeSpecJSON } from "../lib";
 import { useNodesSpec } from "../use-nodes-spec";
 
-export const NodeSideBar = (props: any) => {
-  const [search, setSearch] = useState("");
-  const [nodesSpec] = useNodesSpec();
+const { Panel } = Collapse;
 
-  let filtered = nodesSpec as NodeSpecJSON[];
-  filtered = filtered.filter((node) => {
-    const term = search.toLowerCase();
-    return node.type.toLowerCase().includes(term);
-  });
+export const NodeSideBar = () => {
+  const [nodesSpec] = useNodesSpec();
+  const [search, setSearch] = useState("");
+  const [nodes, setNodes] = useState<{ [key: string]: NodeSpecJSON[] }>({});
+  const [activeKeyPanel, setActiveKeyPanel] = useState<string[]>([]);
+
+  const onChangeSearch = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
+  const onChangeOpenPanels = (key: string | string[]) => {
+    setActiveKeyPanel(typeof key === "string" ? [key] : key);
+  };
 
   const onDragStart = (event: any, isParent: any, nodeType: string) => {
     const data = { isParent, nodeType };
@@ -19,8 +26,29 @@ export const NodeSideBar = (props: any) => {
     event.dataTransfer.effectAllowed = "move";
   };
 
+  useEffect(() => {
+    const key = search.toLowerCase().trim();
+    const items = nodesSpec as NodeSpecJSON[];
+    const types = {} as { [key: string]: NodeSpecJSON[] };
+    const filtered =
+      key.length > 0
+        ? items.filter((node) => node.type.toLowerCase().includes(key))
+        : items;
+
+    filtered.forEach((item) => {
+      if (types[item.category]) {
+        types[item.category].push(item);
+      } else {
+        types[item.category] = [item];
+      }
+    });
+
+    setActiveKeyPanel(key.length > 0 ? Object.keys(types) : []);
+    setNodes(types);
+  }, [search, nodesSpec]);
+
   return (
-    <div className="rubix-flow__node-sidebar node-picker z-10 text-white bg-gray-800 border rounded border-gray-500">
+    <div className="rubix-flow__node-sidebar node-picker z-10 text-white bg-gray-800 border border-gray-500">
       <div className="bg-gray-500 p-2">Add Node</div>
       <div className="p-2">
         <input
@@ -29,20 +57,38 @@ export const NodeSideBar = (props: any) => {
           placeholder="Type to filter"
           className=" bg-gray-600 disabled:bg-gray-700 w-full py-1 px-2"
           value={search}
-          onChange={(e) => setSearch(e.target.value)}
+          onChange={onChangeSearch}
         />
       </div>
-      <div className="overflow-y-scroll" style={{ maxHeight: "90vh" }}>
-        {filtered.map(({ type, isParent }) => (
-          <div
-            key={type}
-            className="p-2 cursor-pointer border-b border-gray-600"
-            onDragStart={(event) => onDragStart(event, isParent, type)}
-            draggable
-          >
-            {type}
-          </div>
-        ))}
+      <div
+        className="overflow-y-scroll"
+        style={{ height: "calc(100vh - 70px)" }}
+      >
+        <Collapse
+          activeKey={activeKeyPanel}
+          expandIconPosition="right"
+          onChange={onChangeOpenPanels}
+        >
+          {Object.keys(nodes).map((category) => (
+            <Panel
+              key={category}
+              header={category}
+              className="panel-no-padding"
+            >
+              {nodes[category].map(({ type, isParent }) => (
+                <div
+                  key={type}
+                  className="py-2 cursor-pointer border-b border-gray-600 text-left"
+                  onDragStart={(event) => onDragStart(event, isParent, type)}
+                  draggable
+                  style={{ paddingLeft: 24 }}
+                >
+                  {type.split("/")[1]}
+                </div>
+              ))}
+            </Panel>
+          ))}
+        </Collapse>
       </div>
     </div>
   );
