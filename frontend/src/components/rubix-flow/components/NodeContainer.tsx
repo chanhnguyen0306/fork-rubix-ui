6c986b1d6f2dbb60a48e596438b028eaca18d475
Oanh Nguyen <52970495+nguyenoanh5597@users.noreply.github.com>
2022-11-04 17:36:29 +0700

    #291 add node status messages

--- frontend/src/components/rubix-flow/components/NodeContainer.tsx
+++ frontend/src/components/rubix-flow/components/NodeContainer.tsx
@@ -2,6 +2,7 @@ import { PropsWithChildren } from "react";
 import cx from "classnames";
 import { categoryColorMap, colors } from "../util/colors";
 import { NodeSpecJSON } from "../lib";
+import { Tooltip } from "antd";
 
 type NodeProps = {
   title: string;
@@ -30,6 +31,36 @@ export const NodeContainer = ({
     borderColor = "border-gray-800";
   }
 
+  const renderTitle = () => {
+    return (
+      <div
+        style={{
+          justifyContent: "space-between",
+          display: "flex",
+        }}
+      >
+        <span>{title}</span>
+        {status?.activeMessage && renderStatusMessages()}
+      </div>
+    );
+  };
+
+  const renderStatusMessages = () => {
+    return (
+      <span className="ml-4">
+        {status?.waringIcon && (
+          <Tooltip title={status.waringMessage}>{status.waringIcon}</Tooltip>
+        )}
+        {status?.notifyIcon && (
+          <Tooltip title={status.notifyMessage}>{status.notifyIcon}</Tooltip>
+        )}
+        {status?.errorIcon && (
+          <Tooltip title={status.errorMessage}>{status.errorIcon}</Tooltip>
+        )}
+      </span>
+    );
+  };
+
   return (
     <div
       className={cx([
@@ -41,7 +72,7 @@ export const NodeContainer = ({
         className={`${backgroundColor} ${textColor} px-3 py-1 rounded-t`}
         onDoubleClick={onDbClickTitle}
       >
-        {title}
+        {renderTitle()}
         <div>
           {category} {status?.subTitle ? " | " + status.subTitle : null}
         </div>
