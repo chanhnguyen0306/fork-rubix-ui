910806d128587e0fc44112b8f12e62e5dd62c883
hanh.tran <hanh.tv204@gmail.com>
2022-12-19 12:07:44 +0700

    chore: add console log for debugging

--- frontend/src/components/rubix-flow/components/Controls.tsx
+++ frontend/src/components/rubix-flow/components/Controls.tsx
@@ -3,7 +3,6 @@ import { ClearModal } from "./ClearModal";
 import { HelpModal } from "./HelpModal";
 import { LoadModal } from "./LoadModal";
 import { SaveModal } from "./SaveModal";
-import { flowToBehave } from "../transformers/flowToBehave";
 import { useReactFlow } from "react-flow-renderer/nocss";
 import {
   QuestionCircleOutlined,
@@ -13,19 +12,20 @@ import {
   PlayCircleOutlined,
   SettingOutlined,
 } from "@ant-design/icons";
-import { FlowFactory } from "../factory";
 import { useCtrlPressKey } from "../hooks/useCtrlPressKey";
-import { useParams } from "react-router-dom";
-import { handleNodesEmptySettings } from "../util/handleSettings";
 import { FlowSettings, FlowSettingsModal } from "./FlowSettingsModal";
+import { NodeInterface } from "../lib/Nodes/NodeInterface";
 
 type ControlProps = {
-  onDeleteEdges: (nodes: any, edges: any) => void;
+  onDeleteEdges: (nodesDeleted: any, edgesDeleted: any) => void;
   onCopyNodes: (nodes: any) => void;
   onUndo: () => void;
   onRedo: () => void;
   onRefreshValues: () => void;
   settings: FlowSettings;
+  selectedNodeForSubFlow?: NodeInterface;
+  onHandelSaveFlow: () => void;
+  onSaveSubFlow: () => void;
   onSaveSettings: (settings: FlowSettings) => void;
 };
 
@@ -37,32 +37,16 @@ const Controls = ({
   onRefreshValues,
   settings,
   onSaveSettings,
+  selectedNodeForSubFlow,
+  onSaveSubFlow,
+  onHandelSaveFlow,
 }: ControlProps) => {
   const [loadModalOpen, setLoadModalOpen] = useState(false);
   const [saveModalOpen, setSaveModalOpen] = useState(false);
   const [helpModalOpen, setHelpModalOpen] = useState(false);
   const [clearModalOpen, setClearModalOpen] = useState(false);
   const [settingRefreshModalOpen, setSettingRefreshModalOpen] = useState(false);
-  const { connUUID = "", hostUUID = "" } = useParams();
-  const isRemote = connUUID && hostUUID ? true : false;
   const instance = useReactFlow();
-
-  const factory = new FlowFactory();
-
-  const download = async () => {
-    const nodes = instance.getNodes();
-    const edges = instance.getEdges();
-    const graphJson = flowToBehave(nodes, edges);
-    await factory.DownloadFlow(connUUID, hostUUID, isRemote, graphJson, true);
-
-    const newNodes = await handleNodesEmptySettings(
-      connUUID,
-      hostUUID,
-      isRemote,
-      instance.getNodes()
-    );
-    instance.setNodes(newNodes);
-  };
 
   /* Ctrl + e (key): Save Graph */
   useCtrlPressKey("KeyE", () => {
@@ -74,19 +58,16 @@ const Controls = ({
     setLoadModalOpen(true);
   });
 
-  /* Ctrl + d (key): Delete items selected
-  useCtrlPressKey("KeyD", () => {
+  /* Ctrl + Delete (key): Delete items selected  */
+  useCtrlPressKey("Backspace", () => {
     const _nodes = instance.getNodes();
     const _edges = instance.getEdges();
 
-    const newNodes = _nodes.filter((item) => !item.selected);
-    const newEdges = _edges.filter((item) => !item.selected);
+    const nodesDeleted = _nodes.filter((item) => item.selected);
+    const edgesDeleted = _edges.filter((item) => item.selected);
 
-    instance.setNodes(newNodes);
-    instance.setEdges(newEdges);
-
-    onDeleteEdges(newNodes, newEdges);
-  }); */
+    onDeleteEdges(nodesDeleted, edgesDeleted);
+  });
 
   /* Ctrl + a (key): Select all items */
   useCtrlPressKey("KeyA", () => {
@@ -131,12 +112,7 @@ const Controls = ({
     const nodeIdCopied = nodesCopied.map((item) => item.id);
     const edgesCopied = instance
       .getEdges()
-      .filter(
-        (item) =>
-          item.selected &&
-          nodeIdCopied.includes(item.source) &&
-          nodeIdCopied.includes(item.target)
-      );
+      .filter((item) => item.selected && nodeIdCopied.includes(item.source) && nodeIdCopied.includes(item.target));
     onCopyNodes({
       nodes: nodesCopied,
       edges: edgesCopied,
@@ -155,7 +131,7 @@ const Controls = ({
 
   /* Ctrl + S (key): Download/deploy flow */
   useCtrlPressKey("KeyS", () => {
-    download();
+    onHandelSaveFlow();
   });
 
   /* Ctrl + X (key): Refresh node values */
@@ -166,6 +142,15 @@ const Controls = ({
   return (
     <>
       <div className="absolute top-4 right-4 bg-white z-10 flex black--text">
+        {!!selectedNodeForSubFlow && (
+          <div
+            className="cursor-pointer border-r bg-white hover:bg-gray-100 px-8"
+            title="Save sub flow"
+            onClick={onSaveSubFlow}
+          >
+            Save Sub Flow
+          </div>
+        )}
         <div
           className="cursor-pointer border-r bg-white hover:bg-gray-100"
           title="Settings refresh value"
@@ -201,21 +186,14 @@ const Controls = ({
         >
           <RestOutlined className="p-2 text-gray-700 align-middle" />
         </div>
-        <div
-          className="cursor-pointer border-r bg-white hover:bg-gray-100"
-          title="Run"
-          onClick={() => download()}
-        >
+        <div className="cursor-pointer border-r bg-white hover:bg-gray-100" title="Run" onClick={onHandelSaveFlow}>
           <PlayCircleOutlined className="p-2 text-gray-700 align-middle" />
         </div>
       </div>
       <LoadModal open={loadModalOpen} onClose={() => setLoadModalOpen(false)} />
       <SaveModal open={saveModalOpen} onClose={() => setSaveModalOpen(false)} />
       <HelpModal open={helpModalOpen} onClose={() => setHelpModalOpen(false)} />
-      <ClearModal
-        open={clearModalOpen}
-        onClose={() => setClearModalOpen(false)}
-      />
+      <ClearModal open={clearModalOpen} onClose={() => setClearModalOpen(false)} />
       <FlowSettingsModal
         settings={settings}
         open={settingRefreshModalOpen}
